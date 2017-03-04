e32b6d615b6f8cc4b17ae3d34fff7197a29f114c
Deepak Kothandan <deepak.kothandan@valmano.de>
2017-03-04 23:50:33 +0100

    refactor

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './components/Menu';
+import Menu from './components/Main';
 
 ReactDOM.render(
   <Menu />,
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-export default class Menu extends Component {
+export default class Main extends Component {
   render() {
     return (
-      <div className="Menu">
+      <div className="Main">
         Ansible is Awesome
       </div>
     );
