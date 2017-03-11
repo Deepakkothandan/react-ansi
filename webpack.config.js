3f1d4a9f05c25a4f3447d916150ae2ddcd4ea426
Deepak Kothandan <deepak.kothandan@valmano.de>
2017-03-11 14:21:19 +0100

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -5,87 +5,87 @@ const WebpackBrowserPlugin = require('webpack-browser-plugin');
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-	devtool: 'source-map',
-	entry: [
-		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./app/app.jsx'
-	],
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app/app.jsx'
+  ],
 
-	output: {
-		path: path.join(__dirname + '/dist'),
-		filename: "bundle.js",
-	},
+  output: {
+    path: path.join(__dirname + '/dist'),
+    filename: "bundle.js",
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: [/node_modules/],
-				enforce: 'pre',
-				use: [{
-					loader: 'eslint-loader',
-					options: {
-						emitWarning: true,
-					}
-				}]
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: [/node_modules/],
-				use: [{
-					loader: 'babel-loader'
-				}],
-			},
-			{
-				test: /\.scss$/,
-				exclude: [/node_modules/],
-				use: [{
-					loader: 'style-loader'
-				}, {
-					loader: 'css-loader',
-					query: {
-						modules: true,
-						localIdentName: '[name]__[local]___[hash:base64:5]'
-					}
-				}, {
-					loader: 'postcss-loader'
-				}, {
-					loader: 'sass-loader',
-				}]
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        enforce: 'pre',
+        use: [{
+          loader: 'eslint-loader',
+          options: {
+            emitWarning: true,
+          }
+        }]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader'
+        }],
+      },
+      {
+        test: /\.scss$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        }, {
+          loader: 'postcss-loader'
+        }, {
+          loader: 'sass-loader',
+        }]
+      },
+    ],
+  },
 
-	resolve: {
-		extensions: [".js", ".jsx"]
-	},
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Ansi',
-			template: './app/index.html.ejs',
-			inject: 'body',
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-		new WebpackBrowserPlugin({
-			port: 8080,
-			url: 'http://localhost'
-		}),
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				postcss: [
-					autoprefixer(),
-				]
-			}
-		})
-	],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Ansi',
+      template: './app/index.html.ejs',
+      inject: 'body',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new WebpackBrowserPlugin({
+      port: 8080,
+      url: 'http://localhost'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer(),
+        ]
+      }
+    })
+  ],
 
-	devServer: {
-		hot: true,
-		inline: true,
-		contentBase: 'app/',
-		historyApiFallback: true,
-		overlay: true
-	}
+  devServer: {
+    hot: true,
+    inline: true,
+    contentBase: 'app/',
+    historyApiFallback: true,
+    overlay: true
+  }
 }
